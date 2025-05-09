f5d9224b3590daf6ce68be2d198051e37f8db70c
luzhihong <xiaopikaqiu@outlook.com>
2025-05-09 16:16:12 +0800

    feat: add post edit modal page with responsive layout

---
+++ src/app/(pages)/@modal/post-edit/[item]/page.tsx
@@ -0,0 +1,22 @@
+import type { FC } from 'react';
+
+import { PageModal } from '@/app/_components/modal/page-modal';
+import { isNil } from 'lodash';
+import { notFound } from 'next/navigation';
+
+import { PostEditForm } from './form';
+
+export const PostEditPage: FC<{ params: Promise<{ item: string }> }> = async ({ params }) => {
+    const { item } = await params;
+    if (isNil(item)) return notFound();
+
+    return (
+        <PageModal
+            title="edit post"
+            match={['/post-edit/*']}
+            className="tw-min-w-full lg:tw-min-w-[60%]"
+        >
+            <PostEditForm id={item}></PostEditForm>
+        </PageModal>
+    );
+};
