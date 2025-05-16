48968f905b55f8502e3c94d5863e16a598b5f323
luzhihong <xiaopikaqiu@outlook.com>
2025-05-16 09:25:35 +0800

    feat: implement post create form component

---
+++ src/app/(pages)/posts/create/form.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import type { PostCreateFormRef } from '@/app/_components/post/types';
+import type { FC, MouseEventHandler } from 'react';
+
+import { BackButton } from '@/app/_components/home/back-button';
+import { PostActionForm } from '@/app/_components/post/action-form';
+import { Button } from '@/app/_components/shadcn/ui/button';
+import { Save } from 'lucide-react';
+import { useCallback, useRef, useState } from 'react';
+
+import $styles from './style.module.css';
+
+export const PostCreateForm: FC = () => {
+    const ref = useRef<PostCreateFormRef | null>(null);
+    const [pending, setPending] = useState(false);
+    const changePending = useCallback((value: boolean) => {
+        setPending(value);
+    }, []);
+    const createPost = useCallback<MouseEventHandler<HTMLButtonElement>>(async (e) => {
+        e.preventDefault();
+        ref.current?.create && (await ref.current?.create());
+    }, []);
+    return (
+        <>
+            <div className={$styles.actions}>
+                <BackButton></BackButton>
+                <Button onClick={createPost} disabled={pending}>
+                    {pending ? 'Saveing...' : 'Save'}
+                    <Save></Save>
+                </Button>
+            </div>
+            <PostActionForm ref={ref} type="create" setPedding={changePending}></PostActionForm>
+        </>
+    );
+};
