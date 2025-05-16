12fe97852188cccc600277a10b1c1bc013203b6e
luzhihong <xiaopikaqiu@outlook.com>
2025-05-16 09:55:36 +0800

    feat: implement post creation page with SSR

---
+++ src/app/(pages)/posts/create/page.tsx
@@ -0,0 +1,18 @@
+import type { FC } from 'react';
+
+import { PostCreateForm } from './form';
+import $styles from './style.module.css';
+
+//Add dynamic marker to force SSR rendering
+export const dynamic = 'force-dynamic';
+//export const fetchCache = 'force-no-store';
+
+const PostCreatePage: FC = async () => {
+    return (
+        <div className="tw-page-container">
+            <div className={$styles.item}>
+                <PostCreateForm></PostCreateForm>
+            </div>
+        </div>
+    );
+};
